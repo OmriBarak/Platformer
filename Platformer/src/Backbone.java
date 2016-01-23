808fcffe7b3b2755fcd95126b142b29fc20691ec
Omri Barak <omribarak@pitt.edu>
2016-01-23 17:20:48 -0500

    Set opengl version to 4.1

--- Platformer/src/Backbone.java
+++ Platformer/src/Backbone.java
@@ -8,8 +8,8 @@ import static org.lwjgl.system.MemoryUtil.*;
 
 public class Backbone
 {
-	private static final int WIDTH = 300;
-	private static final int HEIGHT = 300;
+	private static final int WIDTH = 1000;
+	private static final int HEIGHT = 500;
 
 	private GLFWErrorCallback errorCallback;
 	private GLFWKeyCallback keyCallback;
@@ -31,23 +31,31 @@ public class Backbone
 
 		glfwDefaultWindowHints();
 		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+		glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+		
 		windowUID = glfwCreateWindow(WIDTH, HEIGHT, "Mario meets Fez meets Journey", NULL, NULL);
 		if (windowUID == NULL ) {
 			System.err.println("Failed to create the GLFW window");
 			assert false;
 		}
 
-		//TODO: implement keybinds in a separate class
 		glfwSetKeyCallback(windowUID, keyCallback = new KeyboardHandler());
 
 		GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
-
 		glfwSetWindowPos(windowUID, (vidmode.width()-WIDTH)/2, (vidmode.height()-HEIGHT)/2);
 		
 		glfwMakeContextCurrent(windowUID);
-		glfwSwapInterval(1);
-		
 		glfwShowWindow(windowUID);
+		
+		GL.createCapabilities();
+		glfwSwapInterval(1);
+		glClearColor(0.0f, 0.0f, 1.0f, 0.0f);
+		glEnable(GL_DEPTH_TEST);
+		
+		System.out.println("Running OpenGL version" + glGetString(GL_VERSION) + ".");
 	}
 
 	private void draw() {
@@ -64,10 +72,7 @@ public class Backbone
 
 	public void run() {
 		init();
-		
-		GL.createCapabilities();
-		glClearColor(0.0f, 0.0f, 1.0f, 0.0f);
-
+	
 		while (glfwWindowShouldClose(windowUID) == GLFW_FALSE ) {
 			glfwPollEvents();
 			draw();
