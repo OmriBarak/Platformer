9904f872e887f575e0ca0933a18b4d67c0778169
Omri Barak <omribarak@pitt.edu>
2016-01-23 18:09:36 -0500

    Updated vertex

--- Platformer/src/Vertex.java
+++ Platformer/src/Vertex.java
@@ -1,9 +1,20 @@
 
-public class Vertex {
-	private long VAOid = 0;
-	private long vboPosition = 0, vboTexUV = 0, vboNormal = 0;
+public class Vertex {	
+	private float[] position;
+	private float[] texUV;
+	private float[] normal;
 	
-	float[] position;
-	float[] texUV;
-	float[] normal;
+	public Vertex() {
+		position = new float[3];
+		texUV = new float[2];
+		normal = new float[3];
+	}
+	
+	public void setPosition(float[] pos)		{ position = pos; }
+	public void setTextureCoords(float[] tex)	{ texUV = tex; }
+	public void setNormal(float[] norm)			{ normal = norm; }
+	
+	public float[] getPosition()		{ return position; }
+	public float[] getTextureCoords()	{ return texUV; }
+	public float[] getNormal()			{ return normal; }
 }
