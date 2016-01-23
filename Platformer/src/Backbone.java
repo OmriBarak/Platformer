34129d09bff6943c25ca440e8f6feced6863d4a4
Omri Barak <omribarak@pitt.edu>
2016-01-23 18:55:48 -0500

    Vertex now works with OpenGL

--- Platformer/src/Backbone.java
+++ Platformer/src/Backbone.java
@@ -52,7 +52,7 @@ public class Backbone
 		
 		GL.createCapabilities();
 		glfwSwapInterval(1);
-		glClearColor(0.0f, 0.0f, 1.0f, 0.0f);
+		GL11.glClearColor(0.0f, 0.0f, 1.0f, 0.0f);
 		glEnable(GL_DEPTH_TEST);
 		System.out.println("Running OpenGL version" + glGetString(GL_VERSION) + ".");
 	}
--- Platformer/src/Vertex.java
+++ Platformer/src/Vertex.java
@@ -1,13 +1,58 @@
+import java.nio.FloatBuffer;
+
+import org.lwjgl.BufferUtils;
+import org.lwjgl.opengl.*;
 
 public class Vertex {	
 	private float[] position;
 	private float[] texUV;
 	private float[] normal;
 	
-	public Vertex() {
-		position = new float[3];
-		texUV = new float[2];
-		normal = new float[3];
+	private int vboPosition = 0, vboTexUV = 0, vboNormal = 0;
+	private int vaoId = 0;
+	
+	public Vertex() {}
+	
+	void genBuffers() {
+		
+        
+        
+        
+        
+        
+        //TO DRAW
+	}
+	
+	void bufferData() {
+		vaoId = GL30.glGenVertexArrays();
+		GL30.glBindVertexArray(vaoId);
+		
+		vboPosition	= GL15.glGenBuffers();
+		FloatBuffer positionBuffer = BufferUtils.createFloatBuffer(position.length);
+        positionBuffer.put(position);
+        positionBuffer.flip();
+        GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vboPosition);
+        GL15.glBufferData(GL15.GL_ARRAY_BUFFER, positionBuffer, GL15.GL_STATIC_DRAW);
+        GL20.glVertexAttribPointer(0, 3, GL11.GL_FLOAT, false, 0, 0);
+        
+        vboTexUV	= GL15.glGenBuffers();
+        FloatBuffer textureUVBuffer = BufferUtils.createFloatBuffer(position.length);
+        textureUVBuffer.put(position);
+        textureUVBuffer.flip();
+        GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vboTexUV);
+        GL15.glBufferData(GL15.GL_ARRAY_BUFFER, textureUVBuffer, GL15.GL_STATIC_DRAW);
+        GL20.glVertexAttribPointer(1, 2, GL11.GL_FLOAT, false, 0, 0);
+        
+        vboNormal	= GL15.glGenBuffers();
+        FloatBuffer normalBuffer = BufferUtils.createFloatBuffer(position.length);
+        normalBuffer.put(position);
+        normalBuffer.flip();
+        GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vboNormal);
+        GL15.glBufferData(GL15.GL_ARRAY_BUFFER, normalBuffer, GL15.GL_STATIC_DRAW);
+        GL20.glVertexAttribPointer(2, 3, GL11.GL_FLOAT, false, 0, 0);
+        
+        GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, 0);
+        GL30.glBindVertexArray(0);
 	}
 	
 	public void setPosition(float[] pos)		{ position = pos; }
