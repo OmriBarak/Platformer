0ebb99e7e95398c7f3071ebdc768a09c3294934c
Omri Barak <omribarak@pitt.edu>
2016-01-23 22:23:07 -0500

    Fixed conflicts

--- Platformer/src/Vertex.java
+++ Platformer/src/Vertex.java
@@ -4,17 +4,19 @@ import org.lwjgl.BufferUtils;
 import org.lwjgl.opengl.*;
 
 public class Vertex {	
-	private float[] position;
-	private float[] texUV;
-	private float[] normal;
+	private float[] position; //xyzw
+	private float[] texUV;	  //uv
+	private float[] normal;	  //ijkw
+	private float[] color;	  //rgba
 	
-	private int vboPosition = 0, vboTexUV = 0, vboNormal = 0;
+	private int vboPosition = 0, vboTexUV = 0, vboNormal = 0, vboColor = 0;
 	private int vaoId = 0;
 	
-	public Vertex(float[] pos, float[] tex, float[] norm) {
+	public Vertex(float[] pos, float[] tex, float[] norm, float[] col) {
 		position = pos;
 		texUV = tex;
 		normal = norm;
+		color = col;
 	}
 	
 	void bufferData() {
@@ -27,10 +29,10 @@ public class Vertex {
         positionBuffer.flip();
         GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vboPosition);
         GL15.glBufferData(GL15.GL_ARRAY_BUFFER, positionBuffer, GL15.GL_STATIC_DRAW);
-        GL20.glVertexAttribPointer(0, 3, GL11.GL_FLOAT, false, 0, 0);
+        GL20.glVertexAttribPointer(0, 4, GL11.GL_FLOAT, false, 0, 0);
         
         vboTexUV	= GL15.glGenBuffers();
-        FloatBuffer textureUVBuffer = BufferUtils.createFloatBuffer(position.length);
+        FloatBuffer textureUVBuffer = BufferUtils.createFloatBuffer(texUV.length);
         textureUVBuffer.put(position);
         textureUVBuffer.flip();
         GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vboTexUV);
@@ -38,12 +40,20 @@ public class Vertex {
         GL20.glVertexAttribPointer(1, 2, GL11.GL_FLOAT, false, 0, 0);
         
         vboNormal	= GL15.glGenBuffers();
-        FloatBuffer normalBuffer = BufferUtils.createFloatBuffer(position.length);
+        FloatBuffer normalBuffer = BufferUtils.createFloatBuffer(normal.length);
         normalBuffer.put(position);
         normalBuffer.flip();
         GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vboNormal);
         GL15.glBufferData(GL15.GL_ARRAY_BUFFER, normalBuffer, GL15.GL_STATIC_DRAW);
-        GL20.glVertexAttribPointer(2, 3, GL11.GL_FLOAT, false, 0, 0);
+        GL20.glVertexAttribPointer(2, 4, GL11.GL_FLOAT, false, 0, 0);
+        
+        vboColor	= GL15.glGenBuffers();
+        FloatBuffer colorBuffer = BufferUtils.createFloatBuffer(color.length);
+        colorBuffer.put(position);
+        colorBuffer.flip();
+        GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vboColor);
+        GL15.glBufferData(GL15.GL_ARRAY_BUFFER, colorBuffer, GL15.GL_STATIC_DRAW);
+        GL20.glVertexAttribPointer(3, 4, GL11.GL_FLOAT, false, 0, 0);
         
         GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, 0);
         GL30.glBindVertexArray(0);
--- Platformer/src/VertexShader.glsl
+++ Platformer/src/VertexShader.glsl
@@ -1,7 +1,7 @@
 #version 330 core
 
 layout(location = 0) in vec4 position;
-layout(location = 1) in vec4 color;
+layout(location = 3) in vec4 color;
 
 out vec4 vColor;
 
